657c152b3c9c85de0081da1525c78111aaf3e4dc
adria960 <adria60@gmail.com>
2026-05-25 02:26:44 +0200

    Update test wrong result.

--- src/test/java/com/javatechie/cicd/GithubCicdActionsApplicationTests.java
+++ src/test/java/com/javatechie/cicd/GithubCicdActionsApplicationTests.java
@@ -1,13 +1,19 @@
 package com.javatechie.cicd;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class GithubCicdActionsApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    public void test1() {
+        Assertions.assertEquals(1, 2);
+    }
 
 }
